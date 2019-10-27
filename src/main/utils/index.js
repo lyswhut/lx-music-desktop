46aacba037d662860013a761811e680ece9225bf
lyswhut <lyswhut@qq.com>
2019-10-27 11:58:55 +0800

    修复没有配置文件时程序启动出错的问题

--- src/main/utils/index.js
+++ src/main/utils/index.js
@@ -3,6 +3,6 @@ const { windowSizeList } = require('../../common/config')
 
 exports.getWindowSizeInfo = () => {
   let electronStore = new Store()
-  const { windowSizeId = 1 } = electronStore.get('setting')
+  const { windowSizeId = 1 } = electronStore.get('setting') || {}
   return windowSizeList.find(i => i.id === windowSizeId) || windowSizeList[0]
 }
