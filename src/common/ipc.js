df653258bb19fba66440985a54772c1b0d8a40be
lyswhut <lyswhut@qq.com>
2019-11-10 15:39:40 +0800

    更换成commonjs写法

--- src/common/ipc.js
+++ src/common/ipc.js
@@ -1,31 +1,31 @@
 const { ipcMain, ipcRenderer } = require('electron')
 
 
-export const mainOn = (event, callback) => {
+exports.mainOn = (event, callback) => {
   ipcMain.on(event, callback)
 }
-export const mainOnce = (event, callback) => {
+exports.mainOnce = (event, callback) => {
   ipcMain.once(event, callback)
 }
 
-export const mainHandle = (name, callback) => {
+exports.mainHandle = (name, callback) => {
   ipcMain.handle(name, callback)
 }
-export const mainHandleOnce = (name, callback) => {
+exports.mainHandleOnce = (name, callback) => {
   ipcMain.handleOnce(name, callback)
 }
 
 
-export const rendererSend = (name, params) => {
+exports.rendererSend = (name, params) => {
   ipcRenderer.send(name, params)
 }
-export const rendererSendSync = (name, params) => ipcRenderer.sendSync(name, params)
+exports.rendererSendSync = (name, params) => ipcRenderer.sendSync(name, params)
 
-export const rendererInvoke = (name, params) => ipcRenderer.invoke(name, params)
+exports.rendererInvoke = (name, params) => ipcRenderer.invoke(name, params)
 
-export const rendererOn = (name, callback) => {
+exports.rendererOn = (name, callback) => {
   ipcRenderer.on(name, callback)
 }
-export const rendererOnce = (name, callback) => {
+exports.rendererOnce = (name, callback) => {
   ipcRenderer.once(name, callback)
 }
