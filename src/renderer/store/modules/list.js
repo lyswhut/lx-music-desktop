8949383ade869b93000b22638fe7c48d34f57b80
lyswhut <lyswhut@qq.com>
2021-12-04 01:55:36 +0800

    修复列表操作问题

--- src/renderer/store/modules/list.js
+++ src/renderer/store/modules/list.js
@@ -345,9 +345,10 @@ const mutations = {
       })
     }
 
-    let list = allList[id]
+    let list = userLists[id]
     if (!list) return
     list.name = name
+    window.eventHub.emit(eventListNames.listChange, [id])
   },
   moveupUserList(state, { id, isSync }) {
     if (!isSync) {
@@ -359,9 +360,10 @@ const mutations = {
 
     const index = userLists.findIndex(l => l.id == id)
     if (index < 0) return
-    let targetList = allList[id]
+    let targetList = userLists[id]
     userLists.splice(index, 1)
     userLists.splice(index - 1, 0, targetList)
+    window.eventHub.emit(eventListNames.listChange, [id])
   },
   movedownUserList(state, { id, isSync }) {
     if (!isSync) {
@@ -372,9 +374,10 @@ const mutations = {
     }
     const index = userLists.findIndex(l => l.id == id)
     if (index < 0) return
-    let targetList = allList[id]
+    let targetList = userLists[id]
     userLists.splice(index, 1)
     userLists.splice(index + 1, 0, targetList)
+    window.eventHub.emit(eventListNames.listChange, [id])
   },
   setMusicPosition(state, { id, position, list, isSync }) {
     if (!isSync) {
