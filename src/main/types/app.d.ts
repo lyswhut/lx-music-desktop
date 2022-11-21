f5cdd99fe6b9a7dd30b12348c5c99efa18260705
lyswhut <lyswhut@qq.com>
2022-11-21 17:32:55 +0800

    更新日志

--- src/main/types/app.d.ts
+++ src/main/types/app.d.ts
@@ -4,6 +4,23 @@
 import { AppType, ListType } from '@main/event'
 import { DBSeriveTypes } from '@main/worker/utils'
 
+interface Lx {
+  appSetting: LX.AppSetting
+  hotKey: {
+    enable: boolean
+    config: LX.HotKeyConfigAll
+    state: LX.HotKeyState
+  }
+  isTrafficLightClose: boolean
+  isQuitting: boolean
+  event_app: AppType
+  event_list: ListType
+  worker: {
+    dbService: DBSeriveTypes
+  }
+  theme: LX.ThemeSetting
+}
+
 declare global {
   // declare module NodeJS {
   //   export interface Global {
@@ -15,22 +32,6 @@ declare global {
   //     }
   //   }
   // }
-  interface Lx {
-    appSetting: LX.AppSetting
-    hotKey: {
-      enable: boolean
-      config: LX.HotKeyConfigAll
-      state: LX.HotKeyState
-    }
-    isTrafficLightClose: boolean
-    isQuitting: boolean
-    event_app: AppType
-    event_list: ListType
-    worker: {
-      dbService: DBSeriveTypes
-    }
-    theme: LX.ThemeSetting
-  }
 
   var isDev: boolean
   var envParams: LX.EnvParams
