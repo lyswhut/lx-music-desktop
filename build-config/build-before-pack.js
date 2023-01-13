84e52f5525647e5348e2c1d64c1981fc043cc22a
lyswhut <lyswhut@qq.com>
2023-01-14 00:06:04 +0800

    修复Linux armv7l系统（如树莓派）下无法启动的问题

--- build-config/build-before-pack.js
+++ build-config/build-before-pack.js
@@ -3,11 +3,16 @@ const fsPromises = require('fs').promises
 const path = require('path')
 const { Arch } = require('electron-builder')
 
-const replaceSqliteLib = async() => {
+const fileNameMap = {
+  [Arch.arm64]: 'arm64',
+  [Arch.armv7l]: 'armv7l',
+}
+
+const replaceSqliteLib = async(arch) => {
   // console.log(await fs.readdir(path.join(context.appOutDir, './resources/')))
   // if (context.electronPlatformName != 'linux' || context.arch != Arch.arm64) return
   console.log('replace sqlite lib...')
-  const filePath = path.join(__dirname, './lib/better_sqlite3.linux.arm64.node')
+  const filePath = path.join(__dirname, `./lib/better_sqlite3.linux.${fileNameMap[arch]}.node`)
   const targetPath = path.join(__dirname, '../node_modules/better-sqlite3/build/Release/better_sqlite3.node')
   await fsPromises.unlink(targetPath).catch(_ => _)
   await fsPromises.copyFile(filePath, targetPath)
@@ -19,13 +24,20 @@ module.exports = async(context) => {
   if (electronPlatformName !== 'linux') return
   const bindingFilePath = path.join(__dirname, '../node_modules/better-sqlite3/binding.gyp')
   const bindingBakFilePath = path.join(__dirname, '../node_modules/better-sqlite3/binding.gyp.bak')
-  if (arch == Arch.arm64) {
-    console.log('rename binding file...')
-    await fsPromises.rename(bindingFilePath, bindingBakFilePath)
-    await replaceSqliteLib()
-  } else {
-    if (fs.existsSync(bindingFilePath)) return
-    console.log('restore binding file...')
-    await fsPromises.rename(bindingBakFilePath, bindingFilePath)
+  switch (arch) {
+    case Arch.arm64:
+    case Arch.armv7l:
+      if (fs.existsSync(bindingFilePath)) {
+        console.log('rename binding file...')
+        await fsPromises.rename(bindingFilePath, bindingBakFilePath)
+      }
+      await replaceSqliteLib(arch)
+      break
+
+    default:
+      if (fs.existsSync(bindingFilePath)) return
+      console.log('restore binding file...')
+      await fsPromises.rename(bindingBakFilePath, bindingFilePath)
+      break
   }
 }
