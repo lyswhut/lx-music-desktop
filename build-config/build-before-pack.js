10bd75ea7b17c059344a9b32ca6566e368991008
lyswhut <lyswhut@qq.com>
2023-02-08 00:27:31 +0800

    更新Electron到v22.2.0

--- build-config/build-before-pack.js
+++ build-config/build-before-pack.js
@@ -4,24 +4,24 @@ const path = require('path')
 const { Arch } = require('electron-builder')
 
 const better_sqlite3_fileNameMap = {
-  [Arch.arm64]: 'arm64.glibc27',
-  [Arch.armv7l]: 'armv7l',
+  [Arch.arm64]: 'electron-v110-linux-arm64',
+  [Arch.armv7l]: 'electron-v110-linux-arm',
 }
 
 const qrc_decode_fileNameMap = {
   win32: {
-    [Arch.x64]: 'windows.x64',
-    [Arch.ia32]: 'windows.x86',
-    [Arch.arm64]: 'windows.arm64',
+    [Arch.x64]: 'electron-v110-win32-x64',
+    [Arch.ia32]: 'electron-v110-win32-ia32',
+    [Arch.arm64]: 'electron-v110-win32-arm64',
   },
   linux: {
-    [Arch.x64]: 'linux.x64',
-    [Arch.arm64]: 'linux.arm64',
-    [Arch.armv7l]: 'linux.armv7l',
+    [Arch.x64]: 'electron-v110-linux-x64',
+    [Arch.arm64]: 'electron-v110-linux-arm64',
+    [Arch.armv7l]: 'electron-v110-linux-arm',
   },
   darwin: {
-    [Arch.x64]: 'mac.x86',
-    [Arch.arm64]: 'mac.arm64',
+    [Arch.x64]: 'electron-v110-darwin-x64',
+    [Arch.arm64]: 'electron-v110-darwin-arm64',
   },
 }
 
@@ -31,7 +31,7 @@ const replaceSqliteLib = async(arch) => {
   // https://github.com/lyswhut/lx-music-desktop/issues/1102
   // https://github.com/lyswhut/lx-music-desktop/issues/1161
   console.log('replace sqlite lib...')
-  const filePath = path.join(__dirname, `./lib/better_sqlite3.linux.${better_sqlite3_fileNameMap[arch]}.node`)
+  const filePath = path.join(__dirname, `./lib/better_sqlite3_${better_sqlite3_fileNameMap[arch]}.node`)
   const targetPath = path.join(__dirname, '../node_modules/better-sqlite3/build/Release/better_sqlite3.node')
   await fsPromises.unlink(targetPath).catch(_ => _)
   await fsPromises.copyFile(filePath, targetPath)
@@ -39,7 +39,7 @@ const replaceSqliteLib = async(arch) => {
 
 const replaceQrcDecodeLib = async(platform, arch) => {
   console.log('replace qrc_decode lib...', platform, qrc_decode_fileNameMap[platform][arch])
-  const filePath = path.join(__dirname, `./lib/qrc_decode.${qrc_decode_fileNameMap[platform][arch]}.node`)
+  const filePath = path.join(__dirname, `./lib/qrc_decode_${qrc_decode_fileNameMap[platform][arch]}.node`)
   const targetPath = path.join(__dirname, '../build/Release/qrc_decode.node')
   const targetDir = path.dirname(targetPath)
   if (fs.existsSync(targetDir)) await fsPromises.unlink(targetPath).catch(_ => _)
