97204ceea85a09bfb8154c2c465f34d32586991b
彭狸花喵 <94218819+helloplhm-qwq@users.noreply.github.com>
2023-02-26 13:15:49 +0800

    修复酷狗码打开用户歌单

--- src/renderer/utils/musicSdk/kg/songList.js
+++ src/renderer/utils/musicSdk/kg/songList.js
@@ -219,9 +219,9 @@ export default {
         'KG-THash': 'network_super_call.cpp:3676261689:379',
         'User-Agent': '',
       },
-      body: { appid: 1001, clientver: 9020, mid: '21511157a05844bd085308bc76ef3343', clienttime: 640612895, key: '36164c4015e704673c588ee202b9ecb8', data: id },
+      body: { appid: 1001, clientver: 10053, mid: '7684a923fe6483296256d7e67545538b', clienttime: 730666688, key: '4eb7f9587236f6fe15cd6fc4d5064ded', data: id },
     })
-    // console.log(songInfo)
+    console.log(songInfo)
     let songList
     let info = songInfo.info
     if (info.userid != null) {
@@ -232,11 +232,12 @@ export default {
           'KG-THash': 'network_super_call.cpp:3676261689:379',
           'User-Agent': '',
         },
-        body: { appid: 1001, clientver: 9020, mid: '21511157a05844bd085308bc76ef3343', clienttime: 640612895, key: '36164c4015e704673c588ee202b9ecb8', data: { id: info.id, type: 3, userid: info.userid, collect_type: 0, page: 1, pagesize: info.count } },
+        body: { appid: 1001, clientver: 10053, mid: '7684a923fe6483296256d7e67545538b', clienttime: 730666688, key: '4eb7f9587236f6fe15cd6fc4d5064ded', data: { id: info.id, type: 3, userid: info.userid, collect_type: info.collect_type, page: 1, pagesize: info.count } },
       })
-      // console.log(songList)
+      console.log(songList)
     }
     let result = await Promise.all(this.createTask((songList || songInfo.list).map(item => ({ hash: item.hash })))).then(([...datas]) => datas.flat())
+    console.log(result)
     return {
       list: this.filterData2(result) || [],
       page: 1,
