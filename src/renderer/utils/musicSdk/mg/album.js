10a6ac28a623e93a84d5581ccee4e4460a01036e
Folltoshe <folltoshe@foxmail.com>
2023-04-04 18:27:27 +0800

    修复mg源评论加载失败

--- src/renderer/utils/musicSdk/mg/album.js
+++ src/renderer/utils/musicSdk/mg/album.js
@@ -4,15 +4,15 @@ export default {
   getAlbum(songInfo, tryNum = 0) {
     let requestObj = httpFetch(`http://app.c.nf.migu.cn/MIGUM2.0/v1.0/content/queryAlbumSong?albumId=${songInfo.albumId}&pageNo=1`, {
       headers: {
-        sign: '46DB65104950B98FE451AD41047CC6C4',
-        timestamp: 1603451430776,
-        appId: 'yyapp2',
-        mode: 'android',
-        ua: 'Android_migu',
-        version: '6.9.4',
-        osVersion: 'android 7.0',
-        'User-Agent': 'okhttp/3.9.1',
-        channel: '0146832',
+        // sign: '46DB65104950B98FE451AD41047CC6C4',
+        // timestamp: 1603451430776,
+        // appId: 'yyapp2',
+        // mode: 'android',
+        // ua: 'Android_migu',
+        // version: '6.9.4',
+        // osVersion: 'android 7.0',
+        // 'User-Agent': 'okhttp/3.9.1',
+        // channel: '0146832',
       },
     })
     requestObj.promise = requestObj.promise.then(({ body }) => {
@@ -22,7 +22,7 @@ export default {
         requestObj.cancelHttp = tryRequestObj.cancelHttp.bind(tryRequestObj)
         return tryRequestObj.promise
       }
-      console.log(body)
+      // console.log(body)
       return body.songList
     })
     return requestObj
