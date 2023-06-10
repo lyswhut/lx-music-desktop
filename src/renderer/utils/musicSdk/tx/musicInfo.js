c50377d115cc2cdd7810b94395d560336b5d2d29
Folltoshe <folltoshe@foxmail.com>
2023-06-10 17:57:10 +0800

    fix

--- src/renderer/utils/musicSdk/tx/musicInfo.js
+++ src/renderer/utils/musicSdk/tx/musicInfo.js
@@ -5,6 +5,11 @@ export const getMusicInfo = (id) => {
   return httpFetch('https://u.y.qq.com/cgi-bin/musicu.fcg', {
     method: 'POST',
     body: {
+      comm: {
+        ct: '19',
+        cv: '1859',
+        uin: '0',
+      },
       req: {
         module: 'music.pf_song_detail_svr',
         method: 'get_song_detail_yqq',
