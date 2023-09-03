3381ad0e0e1239af010651ba0224c69836b34127
lyswhut <lyswhut@qq.com>
2023-09-03 17:29:43 +0800

    修正事件名字

--- src/renderer/views/songList/List/components/OpenListModal.vue
+++ src/renderer/views/songList/List/components/OpenListModal.vue
@@ -49,7 +49,7 @@ const props = defineProps({
   },
 })
 
-const emit = defineEmits(['update:modelValue'])
+const emit = defineEmits(['update:model-value'])
 
 const router = useRouter()
 const route = useRoute()
