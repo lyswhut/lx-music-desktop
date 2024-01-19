c238d4958475e3fd001c74af2046f28e79f04a14
lyswhut <lyswhut@qq.com>
2024-01-19 16:08:37 +0800

    修复潜在自定义源切换问题

--- src/renderer/core/apiSource.ts
+++ src/renderer/core/apiSource.ts
@@ -4,16 +4,19 @@ import { setUserApi as setUserApiAction } from '@renderer/utils/ipc'
 import musicSdk from '@renderer/utils/musicSdk'
 import apiSourceInfo from '@renderer/utils/musicSdk/api-source-info'
 
-
+let prevId = ''
 export const setUserApi = async(apiId: string) => {
+  prevId = apiId
   if (/^user_api/.test(apiId)) {
     qualityList.value = {}
     userApi.status = false
     userApi.message = 'initing'
 
     await setUserApiAction(apiId).then(() => {
+      if (prevId != apiId) return
       apiSource.value = apiId
     }).catch(err => {
+      if (prevId != apiId) return
       console.log(err)
       let api = apiSourceInfo.find(api => !api.disabled)
       if (!api) return
