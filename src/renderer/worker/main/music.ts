77c2e962ecba6819f5288630c9eeeb9a2284b05e
lyswhut <lyswhut@qq.com>
2024-08-23 20:06:17 +0800

    优化本地歌曲内嵌封面过大时的加载方式

--- src/renderer/worker/main/music.ts
+++ src/renderer/worker/main/music.ts
@@ -1,9 +1,30 @@
 import { getLocalMusicFileLyric, getLocalMusicFilePic } from '@renderer/utils/music'
+import path from 'node:path'
+import os from 'node:os'
+import fs from 'node:fs/promises'
+import { checkPath } from '@common/utils/nodejs'
 
+const getTempDir = async() => {
+  const tempDir = path.join(os.tmpdir(), 'lxmusic_temp')
+  if (!await checkPath(tempDir)) {
+    await fs.mkdir(tempDir, { recursive: true })
+  }
+  return tempDir
+}
 
 export const getMusicFilePic = async(filePath: string) => {
   const picture = await getLocalMusicFilePic(filePath)
   if (!picture) return ''
+  if (picture.data.length > 400_000) {
+    try {
+      const tempDir = await getTempDir()
+      const tempFile = path.join(tempDir, path.basename(filePath) + '.' + picture.format.split('/')[1])
+      await fs.writeFile(tempFile, picture.data)
+      return tempFile
+    } catch (err) {
+      console.log(err)
+    }
+  }
   return `data:${picture.format};base64,${Buffer.from(picture.data).toString('base64')}`
 }
 
