ee20a4ffe94350d1526e8211891e811a07d7994f
lyswhut <lyswhut@qq.com>
2024-11-30 11:54:26 +0800

    修复kg id重复导致的问题

--- src/common/utils/tools.ts
+++ src/common/utils/tools.ts
@@ -6,6 +6,14 @@ export const toNewMusicInfo = (oldMusicInfo: any): LX.Music.MusicInfo => {
     albumName: oldMusicInfo.albumName, // 歌曲专辑名称
     picUrl: oldMusicInfo.img, // 歌曲图片链接
   }
+  const newInfo = {
+    id: `${oldMusicInfo.source}_${oldMusicInfo.songmid}`,
+    name: oldMusicInfo.name,
+    singer: oldMusicInfo.singer,
+    source: oldMusicInfo.source,
+    interval: oldMusicInfo.interval,
+    meta: meta as LX.Music.MusicInfoOnline['meta'],
+  }
 
   if (oldMusicInfo.source == 'local') {
     meta.filePath = oldMusicInfo.filePath ?? oldMusicInfo.songmid ?? ''
@@ -27,6 +35,7 @@ export const toNewMusicInfo = (oldMusicInfo: any): LX.Music.MusicInfo => {
     switch (oldMusicInfo.source) {
       case 'kg':
         meta.hash = oldMusicInfo.hash
+        newInfo.id = oldMusicInfo.songmid + '_' + oldMusicInfo.hash
         break
       case 'tx':
         meta.strMediaMid = oldMusicInfo.strMediaMid
@@ -42,14 +51,7 @@ export const toNewMusicInfo = (oldMusicInfo: any): LX.Music.MusicInfo => {
     }
   }
 
-  return {
-    id: `${oldMusicInfo.source as string}_${oldMusicInfo.songmid as string}`,
-    name: oldMusicInfo.name,
-    singer: oldMusicInfo.singer,
-    source: oldMusicInfo.source,
-    interval: oldMusicInfo.interval,
-    meta: meta as LX.Music.MusicInfoOnline['meta'],
-  }
+  return newInfo
 }
 
 export const toOldMusicInfo = (minfo: LX.Music.MusicInfo) => {
