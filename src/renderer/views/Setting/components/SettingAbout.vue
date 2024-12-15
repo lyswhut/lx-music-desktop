a8a00bef72cfda4a077a300764fa8c1b12ec6194
3gf8jv4dv <3gf8jv4dv@gmail.com>
2024-12-15 14:36:40 +0800

    Revise zh-CN language (2) (#2169)
    * Revise zh-CN language (2)
    
    * Apply suggestion from lyswhut

--- src/renderer/views/Setting/components/SettingAbout.vue
+++ src/renderer/views/Setting/components/SettingAbout.vue
@@ -2,45 +2,43 @@
 dt#about {{ $t('setting__about') }}
 dd
   .p.small
-    | 本软件完全免费，代码已开源，开源地址：
+    | 本软件完全免费，代码已开源。开源地址：
     span.hover.underline(:aria-label="$t('setting__click_open')" @click="openUrl('https://github.com/lyswhut/lx-music-desktop#readme')") https://github.com/lyswhut/lx-music-desktop
   .p.small
-    | 最新版网盘下载地址（网盘内有Windows、MAC版）：
-    span.hover.underline(:aria-label="$t('setting__click_open')" @click="openUrl('https://www.lanzoui.com/b0bf2cfa/')") 网盘地址
-    | &nbsp;&nbsp;密码：
-    span.hover(:aria-label="$t('setting__click_copy')" @click="clipboardWriteText('glqw')") glqw
+    | 最新版下载地址：
+    span.hover.underline(:aria-label="$t('setting__click_open')" @click="openUrl('https://github.com/lyswhut/lx-music-desktop/releases')") GitHub Releases
   .p.small
     | 软件的常见问题可转至：
-    span.hover.underline(:aria-label="$t('setting__click_open')" @click="openUrl('https://lyswhut.github.io/lx-music-doc/desktop/faq')") 常见问题
+    span.hover.underline(:aria-label="$t('setting__click_open')" @click="openUrl('https://lyswhut.github.io/lx-music-doc/desktop/faq')") 桌面版常见问题
   .p.small
     strong 本软件没有客服
-    | ，但我们整理了一些常见的使用问题，
-    strong 仔细 仔细 仔细
+    | ，但我们整理了一些常见的使用问题。
+    strong 仔细、仔细、仔细
     | 地阅读常见问题后，
   .p.small
     | 仍有问题可到 GitHub 提交&nbsp;
-    span.hover.underline(:aria-label="$t('setting__click_open')" @click="openUrl('https://github.com/lyswhut/lx-music-desktop/issues')") issue
+    span.hover.underline(:aria-label="$t('setting__click_open')" @click="openUrl('https://github.com/lyswhut/lx-music-desktop/issues')") Issue
+    | 。
   br
   .p.small 由于软件开发的初衷仅是为了对新技术的学习与研究，因此软件直至停止维护都将会一直保持纯净。
   .p.small
-    | 目前本项目的原始发布地址只有&nbsp;
-    strong GitHub
-    | &nbsp;及&nbsp;
-    strong 蓝奏网盘
-    | &nbsp;，其他渠道均为第三方转载发布，可信度请自行鉴别。
-  .p 本项目无微信公众号之类的官方账号，谨防被骗。
+    | 目前本项目的原始发布地址
+    strong 只有&nbsp;GitHub
+    | ，其他渠道均为第三方转载发布，可信度请自行鉴别。
+  .p.small
+    strong 本项目「没有」微信公众号之类的所谓「官方账号」，谨防被骗！
 
   .p.small
     | 你已签署本软件的&nbsp;
     base-btn(min @click="handleShowPact") 许可协议
     | ，协议的在线版本在&nbsp;
     strong.hover.underline(:aria-label="$t('setting__click_open')" @click="openUrl('https://github.com/lyswhut/lx-music-desktop#%E9%A1%B9%E7%9B%AE%E5%8D%8F%E8%AE%AE')") 这里
-    | &nbsp;。
+    | 。
   br
 
-  .p
-    small By：
-    | 落雪无痕
+  .p.small
+    | By:&nbsp;
+    strong 落雪无痕
 </template>
 
 <script>
