d3248b5dc6d218a36374a26e3fd0afd88704b137
3gf8jv4dv <3gf8jv4dv@gmail.com>
2024-12-22 17:39:49 +0800

    Add scrollbar to "open shared playlist" dialog box

--- src/renderer/views/songList/List/components/OpenListModal.vue
+++ src/renderer/views/songList/List/components/OpenListModal.vue
@@ -1,6 +1,6 @@
 <template>
   <material-modal :show="props.modelValue" teleport="#view" width="60%" @close="emit('update:model-value', $event)" @after-enter="$refs.input.focus()">
-    <main :class="$style.main">
+    <main class="scroll" :class="$style.main">
       <h2>{{ $t('songlist__import_input_title') }}</h2>
       <div :class="$style.inputContent">
         <base-selection v-model="source" :class="$style.select" :list="props.sourceList" item-key="id" item-name="name" />
