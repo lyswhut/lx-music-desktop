992cc8ce6d50b2401c1b3b39a3660260e4e11788
Maybe <1913093102@qq.com>
2026-02-28 21:09:27 +0800

    设置位置移动

--- src/renderer/views/Setting/components/SettingBasic.vue
+++ src/renderer/views/Setting/components/SettingBasic.vue
@@ -105,14 +105,6 @@ dd
       id="setting_basic_playbar_progress_style_full" name="setting_basic_playbar_progress_style"
       need :model-value="appSetting['common.playBarProgressStyle']" value="full" :label="$t('setting__basic_playbar_progress_style_full')" @update:model-value="updateSetting({'common.playBarProgressStyle': $event})")
 
-dd
-  h3#basic_list_cover {{ $t('setting__basic_list_cover') }}
-  div
-    .gap-top
-      base-checkbox(id="setting_list_show_cover" :model-value="appSetting['list.isShowCover']" :label="$t('setting__basic_list_show_cover')" @update:model-value="updateSetting({'list.isShowCover': $event})")
-    .gap-top(v-if="appSetting['list.isShowCover']")
-      base-selection.gap-left(:list="coverSizeList" :model-value="appSetting['list.coverSize']" item-key="id" item-name="label" @update:model-value="updateSetting({'list.coverSize': $event})")
-
 ThemeSelectorModal(v-model="isShowThemeSelectorModal")
 ThemeEditModal(v-model="isShowThemeEditModal" :theme-id="editThemeId" @submit="handleRefreshTheme")
 play-timeout-modal(v-model="isShowPlayTimeoutModal")
@@ -332,16 +324,6 @@ export default {
       ]
     })
 
-    const coverSizeList = computed(() => {
-      return [
-        { id: 32, label: t('setting__basic_list_cover_size_32px') },
-        { id: 44, label: t('setting__basic_list_cover_size_44px') },
-        { id: 56, label: t('setting__basic_list_cover_size_56px') },
-        { id: 72, label: t('setting__basic_list_cover_size_72px') },
-      ]
-    })
-
-
     return {
       appSetting,
       updateSetting,
@@ -373,7 +355,6 @@ export default {
       editThemeId,
       handleEditTheme,
       fontSizeList,
-      coverSizeList,
     }
   },
 }
--- src/renderer/views/Setting/components/SettingList.vue
+++ src/renderer/views/Setting/components/SettingList.vue
@@ -9,6 +9,13 @@ dd
     base-checkbox(id="setting_list_scroll_enable" :model-value="appSetting['list.isSaveScrollLocation']" :label="$t('setting__list_scroll')" @update:model-value="updateSetting({'list.isSaveScrollLocation': $event})")
   .gap-top
     base-checkbox(id="setting_list_clickAction_enable" :model-value="appSetting['list.isClickPlayList']" :label="$t('setting__list_click_action')" @update:model-value="updateSetting({'list.isClickPlayList': $event})")
+dd(:aria-label="$t('setting__list_cover_title')")
+  h3#list_cover {{ $t('setting__list_cover') }}
+  div
+    .gap-top
+      base-checkbox(id="setting_list_show_cover" :model-value="appSetting['list.isShowCover']" :label="$t('setting__list_show_cover')" @update:model-value="updateSetting({'list.isShowCover': $event})")
+    .gap-top(v-if="appSetting['list.isShowCover']")
+      base-selection.gap-left(:list="coverSizeList" :model-value="appSetting['list.coverSize']" item-key="id" item-name="label" @update:model-value="updateSetting({'list.coverSize': $event})")
 dd(:aria-label="$t('setting__basic_sourcename_title')")
   h3#list_addMusicLocationType {{ $t('setting__list_add_music_location_type') }}
   div
@@ -25,14 +32,28 @@ dd(:aria-label="$t('setting__basic_sourcename_title')")
 
 <script>
 // import { ref, onBeforeUnmount } from '@common/utils/vueTools'
+import { computed } from '@common/utils/vueTools'
+import { useI18n } from '@root/lang'
 import { appSetting, updateSetting } from '@renderer/store/setting'
 
 export default {
   name: 'SettingList',
   setup() {
+    const t = useI18n()
+
+    const coverSizeList = computed(() => {
+      return [
+        { id: 32, label: t('setting__list_cover_size_32px') },
+        { id: 44, label: t('setting__list_cover_size_44px') },
+        { id: 56, label: t('setting__list_cover_size_56px') },
+        { id: 72, label: t('setting__list_cover_size_72px') },
+      ]
+    })
+
     return {
       appSetting,
       updateSetting,
+      coverSizeList,
     }
   },
 }
