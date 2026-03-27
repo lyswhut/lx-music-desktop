24a900cd9fc442262f9ccccb14d067c34522fd4d
life <2729783997@qq.com>
2026-03-27 20:10:30 +0800

    feat(setting): 新增自定义源自动更新配置

--- src/common/defaultSetting.ts
+++ src/common/defaultSetting.ts
@@ -22,6 +22,7 @@ const defaultSetting: LX.AppSetting = {
   'common.transparentWindow': !isMac,
   'common.tryAutoUpdate': true,
   'common.showChangeLog': true,
+  'common.autoUpdate': true,
 
   'player.startupAutoPlay': false,
   'player.togglePlayMethod': 'listLoop',
