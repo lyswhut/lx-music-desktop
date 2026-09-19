a04cf3b3071b0878264019f19d7a0cbc68364db8
3gf8jv4dv <3gf8jv4dv@gmail.com>
2026-09-19 08:39:57 +0800

    fix: correct wrong product name on win legacy prompt (#2978)

--- src/main/utils/winLegacy.ts
+++ src/main/utils/winLegacy.ts
@@ -20,7 +20,7 @@ const showWinLegacyMessage = () => {
     const result = dialog.showMessageBoxSync({
       type: 'warning',
       title: 'Windows 旧版提示',
-      message: '您正在使用专为旧版 Windows 准备的 Any Listen，它仅用于实在无法升级到新版系统的用户。\n该版本缺乏安全更新，可能存在安全风险，并且功能未经测试，可能存在兼容性问题。\n\n如果您的设备支持 Windows 10 或更高版本，强烈建议升级系统，切换到标准版本的 Any Listen，以获得更好的安全性和性能。',
+      message: '您正在使用专为旧版 Windows 准备的 LX Music，它仅用于实在无法升级到新版系统的用户。\n该版本缺乏安全更新，可能存在安全风险，并且功能未经测试，可能存在兼容性问题。\n\n如果您的设备支持 Windows 10 或更高版本，强烈建议升级系统，切换到标准版本的 LX Music，以获得更好的安全性和性能。',
       buttons: ['知道了'],
     })
     if (result === 0) {
@@ -30,7 +30,7 @@ const showWinLegacyMessage = () => {
     const result = dialog.showMessageBoxSync({
       type: 'warning',
       title: '您正在使用旧版',
-      message: '您正在使用专为旧版 Windows 准备的 Any Listen，它仅用于实在无法升级到新版系统的用户，但您的系统似乎在标准版的支持范围内，为了获得最佳体验，请切换到 Any Listen 的标准版本。\n\n注意：当前版本缺乏安全更新，可能存在安全风险，并且功能未经测试，可能存在兼容性问题。',
+      message: '您正在使用专为旧版 Windows 准备的 LX Music，它仅用于实在无法升级到新版系统的用户，但您的系统似乎在标准版的支持范围内，为了获得最佳体验，请切换到 LX Music 的标准版本。\n\n注意：当前版本缺乏安全更新，可能存在安全风险，并且功能未经测试，可能存在兼容性问题。',
       buttons: ['跳转新版发布页', '知道了'],
     })
     if (result === 0) {
